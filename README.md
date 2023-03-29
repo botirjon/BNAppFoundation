b5fa11b9f193206a4279baf712d08fdc5260efc0
botirjon <botirjon.nasridinov@gmail.com>
2023-03-29 10:28:01 +0500

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# BNAppFoundationPackage
+
+A description of this package.
---
+++ Sources/BNAppFoundationPackage/BNAppFoundationPackage.swift
@@ -0,0 +1,6 @@
+public struct BNAppFoundationPackage {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
