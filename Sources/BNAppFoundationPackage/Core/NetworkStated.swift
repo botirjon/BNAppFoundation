92a68890522509727181c908fe9e749003e36ccf
botirjon <botirjon.nasridinov@gmail.com>
2023-04-15 23:20:55 +0500

    + networkstated

---
+++ Sources/BNAppFoundationPackage/Core/NetworkStated.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkStated.swift
+//  
+//
+//  Created by Botirjon Nasridinov on 15/04/23.
+//
+
+import Foundation
+
+public enum NetworkStated<T> {
+    case ready(T)
+    case loading
+    case failed(_ reason: String)
+}
